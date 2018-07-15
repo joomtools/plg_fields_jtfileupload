4043afa1b4e551f524fcd5f32d73b971afe151e8
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2018-07-15 21:18:52 +0200

    Get Fieldnames of Fields uses JtFileUpload

--- src/plugins/fields/jtfileupload/jtfileupload.php
+++ src/plugins/fields/jtfileupload/jtfileupload.php
@@ -17,110 +17,136 @@ JFormHelper::addRulePath(JPATH_PLUGINS . '/fields/foo/rules');
 //JForm::getInstance()->validate($submitedValues);
 
 /**
- * Foo plugin.
+ * JtFileUpload plugin.
  *
- * @package  [PACKAGE_NAME]
- * @since    1.0
+ * @package      Joomla.Plugin
+ * @subpackage   Fields.Jtfieldupload
+ *
+ * @since        __DEPLOY_VERSION__
  */
 class plgFieldsJtfileupload extends FieldsPlugin
 {
 	/**
+	 * Fieldname
+	 *
+	 * @var     Array
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected $fieldname = [];
+
+	/**
 	 * Application object
 	 *
-	 * @var    CMSApplication
-	 * @since  1.0
+	 * @var     CMSApplication
+	 * @since   __DEPLOY_VERSION__
 	 */
 	protected $app;
 
 	/**
 	 * Database object
 	 *
-	 * @var    JDatabaseDriver
-	 * @since  1.0
+	 * @var     JDatabaseDriver
+	 * @since   __DEPLOY_VERSION__
 	 */
 	protected $db;
 
 	/**
 	 * Affects constructor behavior. If true, language files will be loaded automatically.
 	 *
-	 * @var    boolean
-	 * @since  1.0
+	 * @var     boolean
+	 * @since   __DEPLOY_VERSION__
 	 */
 	protected $autoloadLanguage = true;
 
 	/**
 	 * Transforms the field into a DOM XML element and appends it as a child on the given parent.
 	 *
-	 * @param   stdClass   $field  The field.
-	 * @param   DOMElement $parent The field node parent.
-	 * @param   JForm      $form   The form.
+	 * @param   stdClass    $field   The field.
+	 * @param   DOMElement  $parent  The field node parent.
+	 * @param   JForm       $form    The form.
 	 *
-	 * @return  DOMElement
+	 * @return   DOMElement
 	 *
-	 * @since   1.0
+	 * @since   __DEPLOY_VERSION__
 	 */
 	public function onCustomFieldsPrepareDom($field, DOMElement $parent, JForm $form)
 	{
-	    $script = "jQuery(document).ready(function(){jQuery('form#adminForm').attr('enctype','multipart/form-data');});";
-        JFactory::getDocument()->addScriptDeclaration($script);
+		// Set Fieldname
+		if ($field->type == 'jtfileupload')
+		{
+			$this->fieldname[] = $field->name;
+		}
 
-        $fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
-        if (!$fieldNode)
-        {
-            return $fieldNode;
-        }
+		// Add enctype to formtag
+		$script = "jQuery(document).ready(function($){ 
+	                    $('form#adminForm').attr('enctype','multipart/form-data');
+	               });";
 
-        $fieldNode->setAttribute('accept', '.pdf,.PDF');
+		JFactory::getDocument()->addScriptDeclaration($script);
 
-        return $fieldNode;
+		$fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
+
+		if (!$fieldNode)
+		{
+			return $fieldNode;
+		}
+
+		$fieldNode->setAttribute('accept', '.pdf,.PDF');
+
+		return $fieldNode;
 	}
 
 	/**
 	 * The save event.
 	 *
-	 * @param   string  $context The context
-	 * @param   JTable  $item    The table
-	 * @param   boolean $isNew   Is new item
-	 * @param   array   $data    The validated data
+	 * @param   string   $context  The context
+	 * @param   JTable   $item     The table
+	 * @param   boolean  $isNew    Is new item
+	 * @param   array    $data     The validated data
 	 *
-	 * @return  boolean
+	 * @return   boolean
 	 *
 	 * @since   3.7.0
 	 */
 	public function onContentBeforeSave($context, $item, $isNew, $data = array())
 	{
-        $input = $this->app->input;
-        $files = $input->files;
+		// Array with fieldnames uses jtfileupload
+		$fieldname = $this->fieldname;
+		$input     = $this->app->input;
+		$files     = $input->files;
 
-     print_r($this->params->get('type'));
-     echo '<p>name</p>';
-        //$files->get
-        if (!empty($_FILES))
-        {
-            //print_r($_FILES);
-
-
-
-            echo '<p>';
-        //    $jinput          = new \Jtf\Input\Files;
-        //    $submitedFiles     = $jinput->get($formTheme);
-         //   $submitedValues = array_merge_recursive($submitedValues, $submitedFiles);
-        }else{echo '<p>empty</p>';};
-       // $this->getForm()->bind($submitedValues);
-        //$this->setFieldValidates();
-        //$valid = $this->getForm()->validate($submitedValues);
+		print_r($this->params->get('type'));
+		echo '<p>name</p>';
+		//$files->get
+		if (!empty($_FILES))
+		{
+			//print_r($_FILES);
 
 
-print_r($context);
+			echo '<p>';
+			//    $jinput          = new \Jtf\Input\Files;
+			//    $submitedFiles     = $jinput->get($formTheme);
+			//   $submitedValues = array_merge_recursive($submitedValues, $submitedFiles);
+		}
+		else
+		{
+			echo '<p>empty</p>';
+		};
+		// $this->getForm()->bind($submitedValues);
+		//$this->setFieldValidates();
+		//$valid = $this->getForm()->validate($submitedValues);
 
-echo "<p>";
 
-print_r($item);
-        echo "<p>";
+		print_r($context);
 
-        print_r($isNew);
-        echo "<p>";
-print_r($data);
+		echo "<p>";
+
+		print_r($item);
+		echo "<p>";
+
+		print_r($isNew);
+		echo "<p>";
+		print_r($data);
 //die();
 	}
 }
